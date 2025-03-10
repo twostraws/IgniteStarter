0d75a24b643c2564ceb605e1782231037f20e1b2
Paul Hudson <paul.hudson@gmail.com>
2025-03-10 08:50:50 +0000

    Updates for new API.

--- Sources/Layouts/MainLayout.swift
+++ Sources/Layouts/MainLayout.swift
@@ -3,7 +3,7 @@ import Ignite
 
 struct MainLayout: Layout {
     var body: some HTML {
-//        HTML { } // Required only if you need to add extra functionality, like analytics
+
         Body {
             content
             IgniteFooter()
